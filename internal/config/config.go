2c4a5a039742e3a7eecdf0d9a5ba1a6ba91aace5
deimossy <deimossy@student.21-school.ru>
2025-09-17 20:26:07 +0500

    chore: init architecture for config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,2 +1,26 @@
 package config
 
+import (
+	"log"
+	"os"
+	"sync"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	AppPort     string
+	AppHost     string
+	once        sync.Once
+)
+
+func LoadConfig() {
+	once.Do(func() {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Println("Error loading .env file")
+		}
+		AppPort = os.Getenv("APP_PORT")
+		AppHost = os.Getenv("APP_HOST")
+	})
+}
