dc05990ca7a690afbecb4e81bcb0fab25ab71cc6
deimossy <deimossy@student.21-school.ru>
2025-09-27 20:05:08 +0500

    feat(handlers): add router with logging route

--- internal/handlers/router.go
+++ internal/handlers/router.go
@@ -1,2 +1,20 @@
 package handlers
 
+import (
+	"net/http"
+
+	"github.com/deimossy/wb-order-service/internal/services/order_service"
+	"github.com/gorilla/mux"
+	"go.uber.org/zap"
+)
+
+func NewRouter(svc order_service.OrderService, logger *zap.Logger) http.Handler {
+	r := mux.NewRouter()
+
+	h := NewOrderHandler(svc, logger)
+
+	r.HandleFunc("/order/{order_uid}", h.GetOrder).Methods(http.MethodGet)
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./ui/")))
+
+	return r
+}
