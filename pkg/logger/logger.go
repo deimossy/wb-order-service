038d669d59d4ac049a3935304cba99c95bc3b717
deimossy <deimossy@student.21-school.ru>
2025-09-27 20:02:40 +0500

    feat(pkg/logger): add initialization of zap logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,2 +1,23 @@
 package logger
 
+import (
+	"log"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func New() *zap.Logger {
+	cfg := zap.NewProductionConfig()
+	cfg.EncoderConfig.TimeKey = "timestamp"
+	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	cfg.EncoderConfig.LevelKey = "level"
+	cfg.EncoderConfig.CallerKey = "caller"
+
+	logger, err := cfg.Build()
+	if err != nil {
+		log.Fatalf("failed to initialize logger: %v", err)
+	}
+
+	return logger
+}
